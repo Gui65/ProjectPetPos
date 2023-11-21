e9a07353cf70296ef0ee78e0eef310bb51bb1b38
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2023-11-21 20:01:59 -0300

    Adicionando todas as minhas alterações

---
+++ usuarios_pettech/usuarios_pettech/src/main/java/br/com/fiap/api/usuarios_pettech/dto/UsuarioDTO.java
@@ -0,0 +1,24 @@
+package br.com.fiap.api.usuarios_pettech.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+public record UsuarioDTO(
+        Long id,
+
+        @NotBlank(message = "Nome não pode estar em branco;")
+        String nome,
+
+        @Email(message = "E-mail inválido;")
+        String email,
+
+        @CPF(message = "CPF inválido;")
+        String cpf,
+
+
+        LocalDate dataNascimento
+){
+}
